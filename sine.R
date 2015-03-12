d8f441782772884c30edf818b050ed413787963c
Charlie FG <charlie@carrotsrc.org>
2015-03-12 12:20:08 +0000

    Added basic sine block

---
+++ sine.R
@@ -0,0 +1,12 @@
+# generate sine wave of frequency f0, at sample rate fs
+# with phase offset of phi, and amplitude of A
+generate_sine <- function(f0, A, x, phi = 0) {
+	wave = A*sin(2*pi*f0*x+phi)
+	return(wave)
+}
+
+generate_samples <- function(fs, seconds=1) {
+	u <- 1/fs
+	v <- u*fs*seconds
+	return(seq(0,v,u))
+}
