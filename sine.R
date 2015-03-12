8d6cde8032a4fa03980500519d8dfda818350515
Charlie FG <charlie@carrotsrc.org>
2015-03-12 15:21:30 +0000

    Added comments

--- sine.R
+++ sine.R
@@ -5,20 +5,28 @@ sine.generate <- function(f0, A, x, phi = 0) {
 	return(wave)
 }
 
-generate.samples <- function(fs, seconds=1) {
+# Generate a vector of time of sample
+# rate fs for seconds of time
+time.generate <- function(fs, seconds=1) {
 	u <- 1/fs
 	v <- u*fs*seconds
 	return(seq(0,v,u))
 }
 
+# Calculate angular frequency/speed of
+# wave of frequency f0
 sine.freqAngular <- function(f0) {
 	return(2*pi*f0)
 }
 
+# Calculate instaneous phase of a wave of f0
+# frequency, at time t with a phase of theta
 sine.phaseInst <- function(f0, t, theta = 0) {
 	return( sine.freqAngular(f0)*t+theta )
 }
 
+# Calculate instantaeous phase of wave of f0
+# frequency at time t with phase theta 0
 sine.freqInstAngular <- function(f0, t, theta = 0) {
 	return( (sine.phaseInst(f0,t, theta)/(2*pi)) )
 }
